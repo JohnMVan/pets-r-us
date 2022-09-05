ecb0e2b57f9a827aea4c69229b5944f781cb00c6
JohnMVan <john.vanhessche@gmail.com>
2022-09-04 19:49:10 -0500

    For pets-r-us first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/* 
+ * ====================================
+ * Title: index.js
+ * Author:  John Vanhessche
+ * DAte: 29 August 2022
+ * Description:  index.js module for pets-r-us app 
+ * ====================================
+ */
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Needed to set path, because the path.join in app.set was throwing a reference error.
+const path = require('path');
+
+//per handout for week 4.  
+app.engine('.html', require('ejs').__express);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static("views"));
+
+//Creating routes for home and grooming pages
+app.get('/', (req, res) => {  
+    res.render("index", {
+        title: "Home page",
+        message: "home page route"
+    })
+});
+
+app.get('/', (req, res) => {
+    res.render("grooming", {
+        title: 'grooming',
+        message: 'grooming page route'
+    })
+});
+
+
+//starting server on port 3000
+app.listen(PORT, () => {
+    console.log('Application started and listening on port ' + PORT);
+})
+
